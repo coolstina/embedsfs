cf4cd935011f00a16a278020789a4ea1bf473c35
helloshaohua <wu.shaohua@foxmail.com>
2022-07-02 12:38:56 +0800

    perf: refactor

--- README.md
+++ README.md
@@ -8,3 +8,88 @@ Golang embedded file system structured processing helper.
 go get -u github.com/coolstina/embedsfs
 ```
 
+## Usage
+
+### Specify embeds relative path
+
+```go
+package main
+
+import (
+	"embed"
+	"fmt"
+	"github.com/coolstina/embedsfs"
+	"log"
+)
+
+//go:embed embeds
+var embeds embed.FS
+
+func main() {
+	// Initialize embedsfs instance.
+	ins := embedsfs.NewEmbedsFS(embeds, embedsfs.WithPath("embeds"))
+
+	// Get golang internal structure embed.FS
+	embeds := ins.Embeds()
+	fmt.Println(embeds)
+
+	// Get golang embeds filesystem filename
+	name := ins.Filename("readme.txt")
+	fmt.Println(name)
+
+	// Get golang embeds filesystem filename
+	data, err := ins.Content("readme.txt")
+	if err != nil {
+		log.Fatalf("get embeds file content to failure: %v", err)
+	}
+
+	fmt.Println(string(data))
+}
+```
+
+### Not specify embeds relative path
+
+```go
+package main
+
+import (
+	"embed"
+	"fmt"
+	"github.com/coolstina/embedsfs"
+	"log"
+)
+
+//go:embed embeds
+var embeds embed.FS
+
+func main() {
+	// Initialize embedsfs instance.
+	ins := embedsfs.NewEmbedsFS(embeds)
+
+	// Get golang internal structure embed.FS
+	embeds := ins.Embeds()
+	fmt.Println(embeds)
+
+	// Get golang embeds filesystem filename
+	name := ins.Filename("embeds/readme.txt")
+	fmt.Println(name)
+
+	// Get golang embeds filesystem filename
+	data, err := ins.Content("embeds/readme.txt")
+	if err != nil {
+		log.Fatalf("get embeds file content to failure: %v", err)
+	}
+
+	fmt.Println(string(data))
+}
+```
+
+## LICENSE
+
+- [Apache License](LICENSE)
+
+## Author
+
+- [@helloshaohua](https://github.com/helloshaohua)
+
+
--- embedsfs_test.go
+++ embedsfs_test.go
@@ -1,3 +1,17 @@
+// Copyright 2022 helloshaohua <wu.shaohua@foxmail.com>;
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
 package embedsfs
 
 import (
@@ -15,6 +29,7 @@ func TestEmbedsFSSuite(t *testing.T) {
 
 type EmbedsFSSuite struct {
 	suite.Suite
+	embedsfsSpecifyPath *EmbedsFS
 	embedsfs *EmbedsFS
 }
 
@@ -22,17 +37,40 @@ type EmbedsFSSuite struct {
 var embeds embed.FS
 
 func (suite *EmbedsFSSuite) BeforeTest(suiteName, testName string) {
-	suite.embedsfs = NewEmbedsFS(embeds, "embeds")
+	suite.embedsfsSpecifyPath = NewEmbedsFS(embeds, WithPath("embeds"))
+	suite.embedsfs = NewEmbedsFS(embeds)
 }
 
-func (suite *EmbedsFSSuite) Test_Filename() {
-	actual := suite.embedsfs.Filename("readme.txt")
+func (suite *EmbedsFSSuite) Test_EmbedsFSSpecifyPath_Filename() {
+	actual := suite.embedsfsSpecifyPath.Filename("readme.txt")
 	assert.Equal(suite.T(), `embeds/readme.txt`, actual)
+
+	actual = suite.embedsfsSpecifyPath.Filename("embeds/readme.txt")
+	assert.Equal(suite.T(), `embeds/readme.txt`, actual)
+
+	actual = suite.embedsfsSpecifyPath.Filename("api/swagger/spec.json")
+	assert.Equal(suite.T(), `embeds/api/swagger/spec.json`, actual)
 }
 
-func (suite *EmbedsFSSuite) Test_FileContent() {
-	actual, err := suite.embedsfs.Content("readme.txt")
+func (suite *EmbedsFSSuite) Test_EmbedsFSSpecifyPath_FileContent() {
+	actual, err := suite.embedsfsSpecifyPath.Content("readme.txt")
 	assert.NoError(suite.T(), err)
 	assert.NotNil(suite.T(), actual)
 	assert.Contains(suite.T(), fmt.Sprintf("%s", actual), "wu.shaohua@foxmail.com")
 }
+
+func (suite *EmbedsFSSuite) Test_EmbedsFS_Filename() {
+	actual := suite.embedsfs.Filename("embeds/readme.txt")
+	assert.Equal(suite.T(), `embeds/readme.txt`, actual)
+
+	actual = suite.embedsfsSpecifyPath.Filename("embeds/api/swagger/spec.json")
+	assert.Equal(suite.T(), `embeds/api/swagger/spec.json`, actual)
+}
+
+func (suite *EmbedsFSSuite) Test_EmbedsFS_FileContent() {
+	actual, err := suite.embedsfs.Content("embeds/readme.txt")
+	assert.NoError(suite.T(), err)
+	assert.NotNil(suite.T(), actual)
+	assert.Contains(suite.T(), fmt.Sprintf("%s", actual), "wu.shaohua@foxmail.com")
+}
+
