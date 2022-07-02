1166296ef5913c98d936a1887bc0fa92c73da77a
HelloShaohua <wu.shaohua@foxmail.com>
2022-07-02 12:39:40 +0800

    perf: refactor (#1)

--- embedsfs.go
+++ embedsfs.go
@@ -1,8 +1,23 @@
+// Copyright 2022 helloshaohua <wu.shaohua@foxmail.com>;
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
 package embedsfs
 
 import (
 	"embed"
 	"path/filepath"
+	"strings"
 )
 
 // EmbedsFS custom definition structure.
@@ -12,8 +27,13 @@ type EmbedsFS struct {
 }
 
 // NewEmbedsFS returns a new EmbedsFS instance
-func NewEmbedsFS(embeds embed.FS, path string) *EmbedsFS {
-	return &EmbedsFS{embeds: embeds, path: path}
+func NewEmbedsFS(embeds embed.FS, options ...Option) *EmbedsFS {
+	ops := &option{}
+	for _, o := range options {
+		o.apply(ops)
+	}
+
+	return &EmbedsFS{embeds: embeds, path: ops.path}
 }
 
 // Embeds returns a embeds filesystem structure
@@ -33,5 +53,13 @@ func (embeds *EmbedsFS) Content(name string) ([]byte, error) {
 
 // Filename returns embed filename contains embed.FS fill path
 func (embeds *EmbedsFS) Filename(name string) string {
-	return filepath.Join(embeds.path, name)
+	var full = name
+	if embeds.path != "" {
+		has := strings.HasPrefix(full, embeds.path)
+		if !has {
+			full = filepath.Join(embeds.path, full)
+		}
+	}
+
+	return full
 }
---
+++ example/case2/main.go
@@ -0,0 +1,46 @@
+// Copyright 2022 helloshaohua <wu.shaohua@foxmail.com>;
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package main
+
+import (
+	"embed"
+	"fmt"
+	"github.com/coolstina/embedsfs"
+	"log"
+)
+
+//go:embed embeds
+var embeds embed.FS
+
+func main() {
+	// Initialize embedsfs instance.
+	ins := embedsfs.NewEmbedsFS(embeds)
+
+	// Get golang internal structure embed.FS
+	embeds := ins.Embeds()
+	fmt.Println(embeds)
+
+	// Get golang embeds filesystem filename
+	name := ins.Filename("embeds/readme.txt")
+	fmt.Println(name)
+
+	// Get golang embeds filesystem filename
+	data, err := ins.Content("embeds/readme.txt")
+	if err != nil {
+		log.Fatalf("get embeds file content to failure: %v", err)
+	}
+
+	fmt.Println(string(data))
+}
